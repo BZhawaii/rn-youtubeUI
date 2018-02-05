911e8c1cab7c46de3ef4d855a935c24c302f349b
Richard Beese <richardbeese@Richards-MacBook-Air.local>
2018-02-04 21:50:44 -0700

    Init Commit

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.navBar}>
+          <Image source={require('./app/images/youtubeLogo.png')} style={{width: 98, height: 22}} />
+        </View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  navBar: {
+    height: 55,
+    backgroundColor: 'white',
+    elevation: 3,
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
+});
