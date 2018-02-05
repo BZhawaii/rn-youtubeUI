6efdc1b3b24f8091beaa1207b533c9776a8d79e2
Richard Beese <richardbeese@Richards-MacBook-Air.local>
2018-02-04 22:59:00 -0700

    Added fonts

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import {
   StyleSheet,
   Text,
   View,
-  Image
+  Image,
+  TouchableOpacity
 } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -18,8 +20,33 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+
         <View style={styles.navBar}>
           <Image source={require('./app/images/youtubeLogo.png')} style={{width: 98, height: 22}} />
+          <View style={styles.rightNav}>
+            <TouchableOpacity> 
+              <Icon style={styles.navItem} name='search' size={25}  />
+            </TouchableOpacity>
+            <TouchableOpacity> 
+              <Icon name='account-circle' size={25} />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.body} >
+
+        </View>
+
+        <View style={styles.tabBar} >
+          <TouchableOpacity>
+            <Icon  />
+          </TouchableOpacity>
+          <TouchableOpacity>
+
+          </TouchableOpacity>
+          <TouchableOpacity>
+
+          </TouchableOpacity>
         </View>
 
       </View>
@@ -37,6 +64,24 @@ const styles = StyleSheet.create({
     elevation: 3,
     paddingHorizontal: 15,
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  rightNav: {
+    flexDirection: 'row'
+  },
+  navItem: {
+    marginLeft: 25
+  },
+  body: {
+    flex: 1
+  },
+  tabBar: {
+    backgroundColor: 'white',
+    height: 60,
+    borderTopWidth: 0.5,
+    borderColor: '#E5E5E5',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
   }
 });
